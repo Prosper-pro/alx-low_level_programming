460096f000744bd035a069d9f0520129e7280a55
Prosper-pro <prosperhagen1@gmail.com>
2021-06-22 09:31:26 -0700

    low level programming

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 
 /**
- * main - Print the word "Holberton"
- *
- * Return: Always 0 (Success)
+ * main - print Holberton, followed by new line
+ * Return: 0
  */
 
 int main(void)
 {
-  int i;
-  char p[] = "Holberton";
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-  for (i = 0; i < 9; i++)
-    {
-      _putchar(p[i]);
-    }
-  _putchar('\n');
-  return (0);
+	return (0);
 }
