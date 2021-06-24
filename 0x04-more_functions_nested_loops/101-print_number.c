4fbf0c754e11c6c831331dc32b8d1382e949aa54
Prosper-pro <prosperhagen1@gmail.com>
2021-06-24 07:21:34 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,47 +1,35 @@
 #include "holberton.h"
 
 /**
- * print_number - Function that prints an integer.
- * @n: int type number
- * Description: Can only use _putchar to print.
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
  */
+
 void print_number(int n)
 {
-	long m; /* power of 10 */
-	int c; /* boolean check */
-	long num; /* convert int to long */
+	unsigned int tens, digit, positive = n;
+	double t_beg = 1;
 
-	num = n;
-	/* negatives */
-	if (num < 0)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		num *= -1;
-		_putchar('-');
-	}
-
-	/* count up */
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
-
-	/* count down */
-	while (num >= 0)
-	{
-		if (m == 1)
+		if (n < 0)
 		{
-			_putchar(num % 10 + '0');
-			num = -1;
+			positive = n * -1;
+			_putchar('-');
 		}
-		else
+
+		while (t_beg <= positive)
+			t_beg *= 10;
+		tens = t_beg / 10;
+
+		while (tens >= 1)
 		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
 		}
 	}
 }
