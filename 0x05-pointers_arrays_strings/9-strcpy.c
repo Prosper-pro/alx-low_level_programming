1b186e4bb41ffc0564ac2ddb11cc3967c48569ca
Prosper-pro <prosperhagen1@gmail.com>
2021-06-28 09:44:39 -0700

    Arrays, pointers and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
+
+/* another method using pointer notation
+char *_strcpy(char *dest, char *src)
+{
+        while (*src)
+	{
+ 	   *dest = *src;
+	   src++;
+	   dest++;
+	}
+	return (dest);
+}
+*/
