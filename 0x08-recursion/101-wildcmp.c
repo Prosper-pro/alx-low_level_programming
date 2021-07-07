a6d5cee999a6fdd7fcccecb83c6f2829345571d9
Prosper-pro <prosperhagen1@gmail.com>
2021-07-07 07:59:33 -0700

    recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * wildcmp - compare two strings with possible wildcards
+ * @s1: string
+ * @s2: string
+ * Return:int 0 or 1
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, (s2 + 1)));
+	else if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp((s1 + 1), (s2 + 1)));
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, (s2 + 1)));
+		else if (wildcmp(s1, s2 + 1))
+			return (1);
+		else
+			return (wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}
