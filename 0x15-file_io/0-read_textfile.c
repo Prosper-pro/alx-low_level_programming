b169235833a23b9cf162ee56621504af77929c73
Hagenimana Prosper <84607010+Prosper-pro@users.noreply.github.com>
2021-08-10 18:37:17 +0200

    Create 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,50 @@
+#include "main.h"
 #include "holberton.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * read_textfile - reads a text file and prints it to the POSIX
+ * standard output.
  *
- * Return: actual number of letters it could read and print
+ * @filename: file name
+ * @letters: is the number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t lenr, lenw;
+	int file;
+	ssize_t rcount, wcount;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+
+	file = open(filename, O_RDWR);
+	if (file == -1)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
-	lenr = read(fd, buffer, letters);
-	close(fd);
-	if (lenr == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);
-	if (lenr != lenw)
+
+	rcount = read(file, buffer, letters);
+	if (rcount == -1)
 		return (0);
-	return (lenw);
+
+	wcount = write(STDOUT_FILENO, buffer, rcount);
+
+	if (wcount == -1 || rcount != wcount)
+		return (0);
+
+	free(buffer);
+
+	close(file);
+
+	return (wcount);
 }
